61682d94ad17b0f3a3adc90080cb4f5d5309cc77
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-09-02 11:19:55 -0500

    tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import Greeting from "./components/pure/greeting";
-import GreetingF from "./components/pure/greetingF";
+import TaskListComponent from "./components/container/task_list";
+{/* import Greeting from "./components/pure/greeting";
+import GreetingF from "./components/pure/greetingF"; */}
 
 function App() {
   return (
     <div className="App"> 
       <header className="App-header">
         {/* Componente propio Greting.jsx */}
-        <Greeting name="Sergio"></Greeting>
-        <GreetingF name = "Luis"></GreetingF>
+        {/*<Greeting name="Sergio"></Greeting>*/}
+        {/*<GreetingF name = "Luis"></GreetingF>*/}
+        <TaskListComponent></TaskListComponent>
       </header>
     </div>
   );
--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { LEVELS } from '../../models/levels.enum';
+import { Task } from '../../models/task.class';
+import TaskComponent from '../pure/task';
 
 const TaskListComponent = () => {
+    const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
 
-
+    const changeState = (id) => {
+        console.log('TODO: Cambiar estado de una task');
+    }
     return (
         <div>
-            
+            <div>
+                <h1>Your Task:</h1>
+            </div>        
+            {/* TODO: Aplicar un for/Map para renderizar una lista */}
+            <TaskComponent task={defaultTask}></TaskComponent>    
         </div>
     );
 };
