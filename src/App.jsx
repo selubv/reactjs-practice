13107e394e8f3d960ae78826a019e3a95b8dc3bf
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-09-08 08:48:07 -0500

    propiedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import TaskListComponent from "./components/container/task_list";
-{/* import Greeting from "./components/pure/greeting";
-import GreetingF from "./components/pure/greetingF"; */}
+import Componente from "./components/componente";
+import Propiedades from "./components/propiedades";
 
 function App() {
   return (
     <div className="App"> 
       <header className="App-header">
-        {/* Componente propio Greting.jsx */}
-        {/*<Greeting name="Sergio"></Greeting>*/}
-        {/*<GreetingF name = "Luis"></GreetingF>*/}
-        <TaskListComponent></TaskListComponent>
+        <Componente msg = "Hola soy una función"/>
+        <Propiedades
+          cadena = "Esto es una cadena"
+          numero = {24}
+          booleano = {true}
+          arreglo = {[1,2,3]}
+          objeto = {{nombre: "Sergio", correo: "selubv0111bonilla@gmail.com"}}
+          funcion = {(num) => num * num}
+          elementoReact = {<i>Esto es un elemento React</i>}
+          componenteReact = {<Componente msg = "Soy un componente pasado como props"></Componente>}
+        />
       </header>
     </div>
   );
